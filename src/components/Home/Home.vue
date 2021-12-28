9db11fc1db6d77763035add061432321a6b65f79
g7834 <wowdacom.hu@housefun.com.tw>
2021-12-28 06:08:59 +0000

    測試 env 檔

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,27 +1,36 @@
 <template>
-  <PopularArticles></PopularArticles>
+  <PopularArticles :pop-articles="popArticles"></PopularArticles>
   <ArticleList></ArticleList>
   <RelevantArticles></RelevantArticles>
 </template>
 
 <script>
+import { ref } from 'vue';
 import { db } from '../../config/firebaseConfig.js';
-import { collection, getDocs } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 
 export default {
   setup() {
+    const popArticles = ref([]);
     // Get a reference to the database service
 
-    const getArticles = async () => {
-      const querySnapshot = await getDocs(
-        collection(db, 'none-chinese-a-year')
-      );
-      console.log(querySnapshot._firestore);
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-      });
+    const getPopularArticles = async () => {
+      const docRef = doc(db, 'article-lists', 'popular-article-lists');
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        for (const [key, value] of Object.entries(docSnap.data())) {
+          popArticles.value.push(value);
+        }
+      } else {
+        console.log('No such document!');
+      }
     };
-    getArticles();
+    getPopularArticles();
+
+    return {
+      popArticles,
+    };
   },
 };
 </script>
