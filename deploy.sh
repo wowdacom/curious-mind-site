d20115fff01f0f3b0e6ad1ce57244d165f9e9334
g7834 <wowdacom.hu@housefun.com.tw>
2021-12-28 06:08:59 +0000

    測試 env 檔 調整 base url 拿掉 index key 調整 build output 檔案 調整設定

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/none-chinese-a-year
+git push -f git@github.com:wowdacom/none-chinese-a-year.git main:gh-pages
+
+cd -
--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,15 @@ import ViteComponents, {
   ElementPlusResolver,
   VantResolver,
 } from 'unplugin-vue-components/resolvers';
+import path from 'path';
+import process from 'process';
+import fs from 'fs';
+
+const version = +new Date();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/none-chinese-a-year/',
   plugins: [
     vue(),
     WindiCSS(),
@@ -21,6 +27,20 @@ export default defineConfig({
       ],
     }),
   ],
+  build: {
+    manifest: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: `assets/[name]_${version}.js`,
+        chunkFileNames: `assets/[name]_${version}.js`,
+        assetFileNames: `assets/index_${version}.[ext]`,
+      },
+    },
+  },
 });
 
 // your plugin installation
