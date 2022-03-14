0cb23d726ba6741ef3f115dd41221065f58d38c0
g7834 <wowdacom.hu@housefun.com.tw>
2022-03-14 11:12:34 +0100

    有點感覺?

--- src/components/Home/ProjectList.vue
+++ src/components/Home/ProjectList.vue
@@ -1,114 +1,64 @@
 <template>
-  <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap w-full mb-20">
-        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-          <h1
-            class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900"
-          >
-            Pitchfork Kickstarter Taxidermy
-          </h1>
-          <div class="h-1 w-20 bg-indigo-500 rounded"></div>
-        </div>
-        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
-          Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-          gentrify, subway tile poke farm-to-table. Franzen you probably haven't
-          heard of them man bun deep jianbing selfies heirloom prism food truck
-          ugh squid celiac humblebrag.
-        </p>
-      </div>
-      <div class="flex flex-wrap -m-4">
-        <div class="xl:w-1/4 md:w-1/2 p-4">
-          <div class="bg-gray-100 p-6 rounded-lg">
-            <img
-              class="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/720x400"
-              alt="content"
-            />
-            <h3
-              class="tracking-widest text-indigo-500 text-xs font-medium title-font"
-            >
-              SUBTITLE
-            </h3>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
-              Chichen Itza
-            </h2>
-            <p class="leading-relaxed text-base">
-              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-              hexagon disrupt edison bulbche.
-            </p>
-          </div>
-        </div>
-        <div class="xl:w-1/4 md:w-1/2 p-4">
-          <div class="bg-gray-100 p-6 rounded-lg">
-            <img
-              class="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/721x401"
-              alt="content"
-            />
-            <h3
-              class="tracking-widest text-indigo-500 text-xs font-medium title-font"
-            >
-              SUBTITLE
-            </h3>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
-              Colosseum Roma
-            </h2>
-            <p class="leading-relaxed text-base">
-              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-              hexagon disrupt edison bulbche.
-            </p>
-          </div>
-        </div>
-        <div class="xl:w-1/4 md:w-1/2 p-4">
-          <div class="bg-gray-100 p-6 rounded-lg">
-            <img
-              class="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/722x402"
-              alt="content"
-            />
-            <h3
-              class="tracking-widest text-indigo-500 text-xs font-medium title-font"
-            >
-              SUBTITLE
-            </h3>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
-              Great Pyramid of Giza
-            </h2>
-            <p class="leading-relaxed text-base">
-              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-              hexagon disrupt edison bulbche.
-            </p>
-          </div>
-        </div>
-        <div class="xl:w-1/4 md:w-1/2 p-4">
-          <div class="bg-gray-100 p-6 rounded-lg">
-            <img
-              class="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/723x403"
-              alt="content"
-            />
-            <h3
-              class="tracking-widest text-indigo-500 text-xs font-medium title-font"
-            >
-              SUBTITLE
-            </h3>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
-              San Francisco
-            </h2>
-            <p class="leading-relaxed text-base">
-              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-              hexagon disrupt edison bulbche.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <div class="text-gray-600 body-font relative">
+    <canvas id="canvasBackground"></canvas>
+    <section class="month month1">1</section>
+    <section class="month month2">2</section>
+    <section class="month month3">3</section>
+    <section class="month month4">4</section>
+    <section class="month month5">5</section>
+    <section class="month month6">6</section>
+  </div>
 </template>
 
 <script>
-export default {};
+import { onMounted } from 'vue';
+
+export default {
+  setup() {
+    const draw = (positionX = 0, positionY = 0) => {
+      let canvas = document.getElementById('canvasBackground');
+      const vwWidth = window.innerWidth,
+        vwHeight = window.innerHeight,
+        percent = 100;
+
+      canvas.width = Math.round((vwWidth * percent) / 100);
+      canvas.height = Math.round((vwHeight * percent * 6) / 100);
+
+      let shift = (positionX * 100) % canvas.width;
+      console.log(shift);
+
+      if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'rgb(200,0,0)';
+        ctx.fillRect(0 + shift, 0 + positionY, 100, 100);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(80 + shift, 10 + positionY, 100, 100);
+      }
+    };
+
+    const handleScroll = () => {
+      let y = window.scrollY;
+      let x = (y * Math.PI) / 180;
+      draw(x, y);
+    };
+
+    onMounted(() => {
+      document.addEventListener('scroll', handleScroll);
+      draw();
+    });
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+#canvasBackground {
+  position: absolute;
+}
+.month {
+  width: 100%;
+  height: 100vh;
+  border: solid 1px red;
+}
+</style>
