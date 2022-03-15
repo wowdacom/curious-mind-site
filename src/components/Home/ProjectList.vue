043e69492fa09a51b934bd746b14f50a00b906d0
g7834 <wowdacom.hu@housefun.com.tw>
2022-03-15 08:23:55 +0100

    增加位置條件判斷

--- src/components/Home/ProjectList.vue
+++ src/components/Home/ProjectList.vue
@@ -11,11 +11,38 @@
 </template>
 
 <script>
+import { reactive, ref } from 'vue';
 import { onMounted } from 'vue';
 
 export default {
   setup() {
-    const draw = (positionX = 0, positionY = 0) => {
+    const box1 = reactive({
+      x: 0,
+      y: 0,
+      styles: [
+        '#000000',
+        '#808080',
+        '#C0C0C0',
+        '#DCDCDC',
+        '#FFFFFF',
+        '#FFFAFA',
+      ],
+    });
+    const box2 = reactive({
+      x: 10,
+      y: 10,
+      styles: [
+        '#CC99FF',
+        '#A9D1F7',
+        '#B4F0A7',
+        '#FFFFBF',
+        '#FFDFBE',
+        '#FFB1B0',
+      ],
+    });
+
+    const draw = (step) => {
+      console.log(step);
       let canvas = document.getElementById('canvasBackground');
       const vwWidth = window.innerWidth,
         vwHeight = window.innerHeight,
@@ -24,29 +51,56 @@ export default {
       canvas.width = Math.round((vwWidth * percent) / 100);
       canvas.height = Math.round((vwHeight * percent * 6) / 100);
 
-      let shift = positionX * canvas.width;
-      console.log(shift);
-
       if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = 'rgb(200,0,0)';
-        ctx.fillRect(0 + shift, 0 + positionY, 100, 100);
+        ctx.fillStyle = box1.styles[step];
+        ctx.fillRect(box1.x, box1.y, 100, 100);
 
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(80 + shift, 10 + positionY, 100, 100);
+        ctx.fillStyle = box2.styles[step];
+        ctx.fillRect(box2.x, box2.y, 100, 100);
       }
     };
 
     const handleScroll = () => {
       let y = window.scrollY;
       let x = Math.sin((y * Math.PI) / 180);
-      draw(x, y);
+      box1.x = 0;
+      box1.y = y;
+
+      box2.x = 10;
+      box2.y = 10 + y;
+      let step = getStep(y);
+      draw(step);
+    };
+
+    const ranges = ref([]);
+
+    const getBoxRange = () => {
+      document.querySelectorAll('.month').forEach((sectionEl, index) => {
+        let currentRange = {
+          step: index,
+          start: sectionEl.offsetTop,
+          end: sectionEl.offsetTop + sectionEl.offsetHeight,
+        };
+        ranges.value.push(currentRange);
+      });
+    };
+
+    const getStep = (y = 0) => {
+      console.log(y);
+      let currentSection = ranges.value.filter(
+        (item) => item.start <= y && y <= item.end
+      );
+
+      return currentSection[0].step;
     };
 
     onMounted(() => {
       document.addEventListener('scroll', handleScroll);
-      draw();
+      getBoxRange();
+      let step = getStep(window.scrollY);
+      draw(step);
     });
   },
 };
@@ -60,5 +114,9 @@ export default {
   width: 100%;
   height: 100vh;
   border: solid 1px red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 100px;
 }
 </style>
