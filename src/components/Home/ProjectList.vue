9abfd32e9bd9dc5f6b2c9425f584adf9a7aae5c5
g7834 <wowdacom.hu@housefun.com.tw>
2022-03-16 04:41:46 +0100

    加上移動的球

--- src/components/Home/ProjectList.vue
+++ src/components/Home/ProjectList.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="text-gray-600 body-font relative">
     <canvas id="canvasBackground"></canvas>
-    <section class="month month1">1</section>
-    <section class="month month2">2</section>
-    <section class="month month3">3</section>
-    <section class="month month4">4</section>
-    <section class="month month5">5</section>
-    <section class="month month6">6</section>
+    <section class="month month1">
+      1 <button class="btn btn1" @click="handleGetBall(0)">球球來我這1</button>
+    </section>
+    <section class="month month2">
+      2 <button class="btn btn2" @click="handleGetBall(1)">球球來我這2</button>
+    </section>
+    <section class="month month3">
+      3 <button class="btn btn3" @click="handleGetBall(2)">球球來我這3</button>
+    </section>
+    <section class="month month4">
+      4 <button class="btn btn4" @click="handleGetBall(3)">球球來我這4</button>
+    </section>
+    <section class="month month5">
+      5 <button class="btn btn5" @click="handleGetBall(4)">球球來我這5</button>
+    </section>
+    <section class="month month6">
+      6 <button class="btn btn6" @click="handleGetBall(5)">球球來我這6</button>
+    </section>
   </div>
 </template>
 
@@ -47,18 +59,42 @@ export default {
         '#FFB1B0',
       ],
     });
+    const ball = reactive({
+      x: 0,
+      y: 0,
+      styles: [
+        '#CC99FF',
+        '#A9D1F7',
+        '#B4F0A7',
+        '#FFFFBF',
+        '#FFDFBE',
+        '#FFB1B0',
+      ],
+      step: 0,
+    });
+    const isInit = ref(false);
 
     const canvasInit = () => {
+      isInit.value = true;
       vw.canvas = document.getElementById('canvasBackground');
       vw.ctx = vw.canvas.getContext('2d');
 
       vw.canvas.width = Math.round((vw.width * vw.percent) / 100);
       vw.canvas.height = Math.round((vw.height * vw.percent * 6) / 100);
+
+      ball.y = btnRanges.value[ball.step].start + 88;
+
+      draw();
+
+      isInit.value = false;
     };
 
-    const draw = (step) => {
+    const draw = () => {
+      let step = getStep(box1.y);
       if (vw.canvas.getContext) {
         vw.ctx.clearRect(0, 0, vw.canvas.width, vw.canvas.height);
+
+        drawArc(vw.canvas.width / 2, ball.y);
         if (step % 2 === 0) {
           drawBox(box1.x, box1.y, box1.styles[step], 100);
           drawBox(box2.x, box2.y, box2.styles[step], 100);
@@ -92,6 +128,18 @@ export default {
       vw.ctx.restore();
     };
 
+    const drawArc = (x, y, size = 50) => {
+      vw.ctx.save();
+      vw.ctx.beginPath();
+      vw.ctx.arc(x, y, size, 0, 2 * Math.PI);
+      vw.ctx.fillStyle = 'green';
+      vw.ctx.fill();
+      vw.ctx.lineWidth = 5;
+      vw.ctx.strokeStyle = '#003300';
+      vw.ctx.stroke();
+      vw.ctx.restore();
+    };
+
     const handleScroll = () => {
       let y = window.scrollY;
       let x = Math.sin((y * Math.PI) / 180);
@@ -100,11 +148,59 @@ export default {
 
       box2.x = 100;
       box2.y = 100 + y;
-      let step = getStep(box1.y);
-      draw(step);
+
+      draw();
+    };
+
+    const isMoving = ref(false);
+
+    const handleGetBall = (step) => {
+      let current = btnRanges.value[ball.step].start + 88,
+        target = btnRanges.value[step].start + 88;
+
+      if (step > ball.step && isMoving.value === false) {
+        isMoving.value = true;
+        requestAnimationFrame(animate);
+
+        function animate(currentTime) {
+          ball.y = current = current + 10;
+          draw();
+
+          // request another loop of animation
+          if (ball.y < target) {
+            requestAnimationFrame(animate);
+          } else {
+            ball.step = step;
+            isMoving.value = false;
+            return;
+          }
+        }
+      } else if (isMoving.value === false) {
+        isMoving.value = true;
+        requestAnimationFrame(animate);
+
+        function animate(currentTime) {
+          ball.y = current = current - 10;
+          draw();
+
+          // request another loop of animation
+          if (ball.y > target) {
+            requestAnimationFrame(animate);
+          } else {
+            ball.step = step;
+            isMoving.value = false;
+            return;
+          }
+        }
+      } else {
+        console.log('移動中');
+      }
+
+      let counter = ball.y;
     };
 
     const ranges = ref([]);
+    const btnRanges = ref([]);
 
     const getBoxRange = () => {
       document.querySelectorAll('.month').forEach((sectionEl, index) => {
@@ -117,6 +213,17 @@ export default {
       });
     };
 
+    const getButtenRange = () => {
+      document.querySelectorAll('.btn').forEach((sectionEl, index) => {
+        let currentRange = {
+          step: index,
+          start: sectionEl.offsetTop,
+          end: sectionEl.offsetTop + sectionEl.offsetHeight,
+        };
+        btnRanges.value.push(currentRange);
+      });
+    };
+
     const getStep = (y = 0) => {
       let currentSection = ranges.value.filter(
         (item) => item.start <= y && y <= item.end
@@ -126,12 +233,15 @@ export default {
     };
 
     onMounted(() => {
+      getBoxRange();
+      getButtenRange();
       canvasInit();
       document.addEventListener('scroll', handleScroll);
-      getBoxRange();
-      let step = getStep(window.scrollY);
-      draw(step);
     });
+
+    return {
+      handleGetBall,
+    };
   },
 };
 </script>
@@ -139,6 +249,7 @@ export default {
 <style scoped>
 #canvasBackground {
   position: absolute;
+  z-index: 0;
 }
 .month {
   width: 100%;
@@ -147,5 +258,15 @@ export default {
   justify-content: center;
   align-items: center;
   font-size: 100px;
+  z-index: 2;
+}
+.btn {
+  position: relative;
+  z-index: 10;
+}
+.btn:focus,
+.btn.focus {
+  outline: none;
+  box-shadow: 0 0 0 0 rgba(0, 123, 255, 0);
 }
 </style>
