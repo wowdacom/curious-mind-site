e827116a1089743a169141254cf58769ef377c04
g7834 <wowdacom.hu@housefun.com.tw>
2022-07-20 08:30:37 +0100

    修改版面

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -8,20 +8,20 @@
           class="absolute left-0 top-0 w-full h-full flex justify-center items-center text-center"
         >
           <h1
-            class="lg:text-8xl lg:translate-y-2 text-5xl transform -rotate-6 translate-y-8"
+            class="lg:text-8xl lg:translate-y-2 text-5xl transform -rotate-6 translate-y-8 z-10"
           >
             My Happiness Project ^ᗜ^
           </h1>
           <img
-            class="absolute left-[0px] bottom-[-40px] w-[35%]"
+            class="absolute left-[0px] bottom-[-57px] w-[35%]"
             src="https://i.imgur.com/bqLyvxa.png"
             alt=""
           />
-          <div class="absolute right-[40px] bottom-[20px] hinge">
+          <div class="absolute right-[60px] bottom-[-10px] hinge">
             <img
-              width="50"
-              height="50"
-              src="https://i.imgur.com/YxG0z5o.png"
+              width="70"
+              height="70"
+              src="https://i.imgur.com/kCJBz7w.png"
               alt=""
             />
           </div>
@@ -49,25 +49,35 @@ export default {};
 @keyframes hinge {
   0% {
     animation-timing-function: ease-in-out;
+    border: none;
   }
 
   20%,
   60%,
-  80% {
-    transform: rotate3d(0, 0, 1, 170deg);
+  80%,
+  100% {
+    transform: rotate3d(0, 0, 1, -20deg);
+    background: yellow;
+    padding: 10px;
+    border: solid 3px black;
+    border-radius: 5px;
     animation-timing-function: ease-in-out;
   }
 
   40%,
   70% {
-    transform: rotate3d(0, 0, 1, 0deg);
+    transform: rotate3d(0, 0, 1, 40deg);
     animation-timing-function: ease-in-out;
     opacity: 1;
+    border: none;
+    background: transparent;
   }
 }
 
 .hinge {
-  animation: hinge 6s infinite;
+  animation: hinge 6s;
+  animation-iteration-count: 1;
   transform-origin: 50% 50%;
+  animation-fill-mode: forwards;
 }
 </style>
--- src/components/Home/HappinessArticleList.vue
+++ src/components/Home/HappinessArticleList.vue
@@ -4,12 +4,47 @@
       <div class="-my-8 divide-y-2 divide-gray-100">
         <div class="my-4 py-8 flex flex-wrap md:flex-nowrap bg-white">
           <div class="pl-8 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="-semibold title- text-gray-700">Side Project</span>
-            <span class="mt-1 text-gray-500 text-sm">April</span>
+            <span class="-semibold title- text-gray-700"
+              >My Happiness Project</span
+            >
+            <span class="mt-1 text-gray-500 text-sm">七月</span>
           </div>
           <div class="pl-8 md:flex-grow">
             <h2 class="text-2xl -medium text-gray-900 title- mb-2">
-              [ HPP ] 關於自然我想說的，但我沒說
+              [ 第五個月 ] 再想想，我的復古生活
+            </h2>
+            <p class="leading-relaxed">
+              很多東西好像是理所當然這樣子，但我們很少問為什麼?
+            </p>
+            <router-link
+              class="text-indigo-500 inline-flex items-center mt-4 cursor-pointer"
+              :to="`/blog/Meditation bushwick direct trade taxidermy shaman`"
+              >看更多
+              <svg
+                class="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M5 12h14"></path>
+                <path d="M12 5l7 7-7 7"></path>
+              </svg>
+            </router-link>
+          </div>
+        </div>
+        <div class="my-4 py-8 flex flex-wrap md:flex-nowrap bg-white">
+          <div class="pl-8 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+            <span class="-semibold title- text-gray-700"
+              >My Happiness Project</span
+            >
+            <span class="mt-1 text-gray-500 text-sm">六月</span>
+          </div>
+          <div class="pl-8 md:flex-grow">
+            <h2 class="text-2xl -medium text-gray-900 title- mb-2">
+              [ 第四個月 ] 關於自然我想說的，但我沒說
             </h2>
             <p class="leading-relaxed">
               大自然的豐富跟奧秘，從森林到外太空，從一群人到靈魂。既深且廣，最後
@@ -36,12 +71,14 @@
         </div>
         <div class="my-4 py-8 flex flex-wrap md:flex-nowrap bg-white">
           <div class="pl-8 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="-semibold title- text-gray-700">Side Project</span>
-            <span class="mt-1 text-gray-500 text-sm">April</span>
+            <span class="-semibold title- text-gray-700"
+              >My Happiness Project</span
+            >
+            <span class="mt-1 text-gray-500 text-sm">五月</span>
           </div>
           <div class="pl-8 md:flex-grow">
             <h2 class="text-2xl -medium text-gray-900 title- mb-2">
-              [ HPP ] 因為認識複雜，我喜歡上了簡單
+              [ 第三個月 ] 因為認識複雜，我喜歡上了簡單
             </h2>
             <p class="leading-relaxed">
               需要時間，需要有好奇心去探索簡單的可能性，然後才了解自己。
@@ -68,12 +105,14 @@
         </div>
         <div class="my-4 py-8 flex flex-wrap md:flex-nowrap bg-white">
           <div class="pl-8 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="-semibold title- text-gray-700">Side Project</span>
-            <span class="mt-1 text-gray-500 text-sm">April</span>
+            <span class="-semibold title- text-gray-700"
+              >My Happiness Project</span
+            >
+            <span class="mt-1 text-gray-500 text-sm">四月</span>
           </div>
           <div class="pl-8 md:flex-grow">
             <h2 class="text-2xl -medium text-gray-900 title- mb-2">
-              [ HPP ] 幸運自造者，練習當一個幸運的人
+              [ 第二個月 ] 幸運自造者，練習當一個幸運的人
             </h2>
             <p class="leading-relaxed">
               幸運是一種命嗎? 還是掌握在自己手裡? Serendipity mindset
@@ -100,12 +139,14 @@
         </div>
         <div class="my-4 py-8 flex flex-wrap md:flex-nowrap bg-white">
           <div class="pl-8 md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="-semibold title- text-gray-700">Side Project</span>
-            <span class="text-sm text-gray-500">March</span>
+            <span class="-semibold title- text-gray-700"
+              >My Happiness Project</span
+            >
+            <span class="text-sm text-gray-500">三月</span>
           </div>
           <div class="pl-8 md:flex-grow">
             <h2 class="text-2xl -medium text-gray-900 title- mb-2">
-              [ HPP ] 回到呼吸，那些呼吸教會我的事
+              [ 第一個月 ] 回到呼吸，那些呼吸教會我的事
             </h2>
             <p class="leading-relaxed">
               方塊呼吸、穴道導引、慢跑、森林浴，疫情下被忘記的呼吸，幫助我們舒緩壓力、重新找回專心，也可以很有趣!
