b64e2495d923b492dd951c814a752dc74f7276b9
wowdacom <wowdacom@gmail.com>
2023-04-27 05:37:27 +0100

    好難

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <Banner></Banner>
-  <h1 class="text-4xl text-center bg-yellow-300">測試</h1>
+  <h1 class="text-4xl text-center bg-green-300">CS/CD 好難</h1>
   <PopularArticles
     v-if="popArticles.length"
     :pop-articles="popArticles"
